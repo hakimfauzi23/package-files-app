f365fccd5ae13e2f9460ae49f63f6961a78ccbec
Hanif <hanif.hakim@spesolution.com>
2023-10-07 21:14:06 +0700

    Create Package Command

---
+++ src/main/java/com/spe/deliveryworker/command/impl/TestCommand.java
@@ -0,0 +1,38 @@
+package com.spe.deliveryworker.command.impl;
+
+import com.spe.deliveryworker.command.Command;
+import com.spe.deliveryworker.service.FileService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+@Component("test")
+@AllArgsConstructor
+public class TestCommand implements Command {
+
+    private final FileService fileService;
+
+    @Override
+    public void execute(String[] args) {
+
+        if (args.length < 3) {
+            System.out.println("Invalid Command Argument(s)");
+            System.exit(0);
+        }
+
+        String diffFile = args[0];
+        String dstFolder = args[1];
+        String srcFolder = args[2];
+        try {
+            Map<String, ArrayList<String>> testMap = fileService.readDiffFile(diffFile);
+            fileService.createDirectories(dstFolder, testMap);
+            fileService.copyFiles(dstFolder, testMap, srcFolder);
+            fileService.packageDirectories(dstFolder);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
